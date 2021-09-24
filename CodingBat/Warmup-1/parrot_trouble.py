27b26a0cf0ce34d1ac84ece13137ef8decc05f4b
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-24 22:29:58 +0530

    add Parrot Trouble

--- CodingBat/Warmup-1/parrot_trouble.py
+++ CodingBat/Warmup-1/parrot_trouble.py
@@ -4,3 +4,9 @@
 parrot_trouble(True, 6) → True
 parrot_trouble(True, 7) → False
 parrot_trouble(False, 6) → False'''
+
+def parrot_trouble(talking, hour):
+  if talking and (hour < 7 or hour > 20):
+    return True
+  else:
+    return False
