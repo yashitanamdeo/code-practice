83696042e1a08ba3a44b5d05389b1d41d41b2bb2
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-24 23:27:40 +0530

    add Not String

--- CodingBat/Warmup-1/not_string.py
+++ CodingBat/Warmup-1/not_string.py
@@ -4,3 +4,8 @@
 not_string('candy') → 'not candy'
 not_string('x') → 'not x'
 not_string('not bad') → 'not bad' '''
+
+def not_string(str):
+  if len(str) >= 3 and str[:3] == "not":
+    return str
+  return "not " + str
