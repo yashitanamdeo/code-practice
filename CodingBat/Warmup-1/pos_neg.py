290efc515015a95bfdd76b37e51974d507cc6339
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-24 23:23:11 +0530

    add pos_neg

--- CodingBat/Warmup-1/pos_neg.py
+++ CodingBat/Warmup-1/pos_neg.py
@@ -4,3 +4,15 @@
 pos_neg(1, -1, False) → True
 pos_neg(-1, 1, False) → True
 pos_neg(-4, -5, True) → True"""
+
+def pos_neg(a, b, negative):
+  if negative:
+    if (a<0 and b<0):
+      return True
+    else:
+      return False
+  elif ((a>0 and (b<0)) or (b>0 and (a<0))):
+    return True
+  else:
+    return False
+    
