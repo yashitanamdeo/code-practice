8f967d20ca4a1c5e5f0974aca015c74d4e8a8b97
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-25 17:42:59 +0530

    add String Match

--- CodingBat/Warmup-2/string_match.py
+++ CodingBat/Warmup-2/string_match.py
@@ -4,3 +4,12 @@
 string_match('xxcaazz', 'xxbaaz') → 3
 string_match('abc', 'abc') → 2
 string_match('abc', 'axc') → 0'''
+
+def string_match(a, b):
+  count = 0
+  for i in range(min(len(a),len(b))-1):
+    a_sub = a[i:i+2]
+    b_sub = b[i:i+2]
+    if a_sub == b_sub:
+      count += 1
+  return count
