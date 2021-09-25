98b15a44c28abb20406528ea3ba8890ffe66878f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-25 19:46:02 +0530

    add Array Count9

--- CodingBat/Warmup-2/array_count9.py
+++ CodingBat/Warmup-2/array_count9.py
@@ -4,3 +4,10 @@
 array_count9([1, 2, 9]) → 1
 array_count9([1, 9, 9]) → 2
 array_count9([1, 9, 9, 3, 9]) → 3'''
+
+def array_count9(nums):
+  count = 0
+  for num in nums:
+    if num == 9:
+      count += 1
+  return count
