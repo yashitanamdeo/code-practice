23cc7b46104b775213d1f570c1df7b58cb7d9fee
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-25 17:53:16 +0530

    add String Bits

--- CodingBat/Warmup-2/string_bits.py
+++ CodingBat/Warmup-2/string_bits.py
@@ -4,3 +4,10 @@
 string_bits('Hello') → 'Hlo'
 string_bits('Hi') → 'H'
 string_bits('Heeololeo') → 'Hello'"""
+
+def string_bits(str):
+  result = ''
+  for i in range(len(str)):
+    if i%2 == 0:
+      result += str[i]
+  return result
