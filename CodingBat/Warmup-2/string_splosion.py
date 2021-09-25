5503b11f9279ac968e2aa9921a55cefcd6faa75c
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-25 19:28:23 +0530

    add String Splosion

---
+++ CodingBat/Warmup-2/string_splosion.py
@@ -0,0 +1,6 @@
+"""Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+
+string_splosion('Code') → 'CCoCodCode'
+string_splosion('abc') → 'aababc'
+string_splosion('ab') → 'aab'"""
