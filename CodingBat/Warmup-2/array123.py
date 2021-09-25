7e862d7389b0226fa6e7339e66587826a78817ac
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-25 23:01:26 +0530

    add Array123

--- CodingBat/Warmup-2/array123.py
+++ CodingBat/Warmup-2/array123.py
@@ -4,3 +4,9 @@
 array123([1, 1, 2, 3, 1]) → True
 array123([1, 1, 2, 4, 1]) → False
 array123([1, 1, 2, 1, 2, 3]) → True'''
+
+def array123(nums):
+  for i in range(len(nums)-2):
+    if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3:
+      return True
+  return False
