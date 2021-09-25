d35af8ed8969f302ce4ba6c1b58fcd93a6177035
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-25 19:29:02 +0530

    add String Splosion

--- CodingBat/Warmup-2/string_splosion.py
+++ CodingBat/Warmup-2/string_splosion.py
@@ -4,3 +4,10 @@
 string_splosion('Code') → 'CCoCodCode'
 string_splosion('abc') → 'aababc'
 string_splosion('ab') → 'aab'"""
+
+def string_splosion(str):
+  
+  result = ''
+  for i in range(len(str)):
+    result += str[:i+1]
+  return result
