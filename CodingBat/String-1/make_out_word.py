9de9b5ba7f09b3d739b07cc8287956de69972443
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-26 19:28:04 +0530

    add Make Out Word

--- CodingBat/String-1/make_out_word.py
+++ CodingBat/String-1/make_out_word.py
@@ -4,3 +4,7 @@
 make_out_word('<<>>', 'Yay') → '<<Yay>>'
 make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
 make_out_word('[[]]', 'word') → '[[word]]'"""
+
+def make_out_word(out, word):
+  out.split()
+  return out[:2]+word+out[2:]
