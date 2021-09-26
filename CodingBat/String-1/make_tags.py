9d0e19eae9df0c1bce1a7eda0f55c9f3d65ef152
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-26 19:25:05 +0530

    add Make Tags

--- CodingBat/String-1/make_tags.py
+++ CodingBat/String-1/make_tags.py
@@ -4,3 +4,6 @@
 make_tags('i', 'Yay') → '<i>Yay</i>'
 make_tags('i', 'Hello') → '<i>Hello</i>'
 make_tags('cite', 'Yay') → '<cite>Yay</cite>'"""
+
+def make_tags(tag, word):
+  return "<"+tag+">"+word+"</"+tag+">"
