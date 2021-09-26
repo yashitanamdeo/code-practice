e23b3fc2804315e3343915099e589a6e2001cdd8
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-26 21:20:24 +0530

    add Common End

--- CodingBat/List-1/common_end.py
+++ CodingBat/List-1/common_end.py
@@ -4,3 +4,6 @@
 common_end([1, 2, 3], [7, 3]) → True
 common_end([1, 2, 3], [7, 3, 2]) → False
 common_end([1, 2, 3], [1, 3]) → True'''
+
+def common_end(a, b):
+  return(a[0] == b[0] or a[-1]==b[-1])
