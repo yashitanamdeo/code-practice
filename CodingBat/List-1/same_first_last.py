798d95ebee5246664d4d2fc5abd9bdb4996a0c53
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-26 20:35:20 +0530

    add Same First Last

--- CodingBat/List-1/same_first_last.py
+++ CodingBat/List-1/same_first_last.py
@@ -4,3 +4,6 @@
 same_first_last([1, 2, 3]) → False
 same_first_last([1, 2, 3, 1]) → True
 same_first_last([1, 2, 1]) → True'''
+
+def same_first_last(nums):
+  return (len(nums)>=1 and nums[0] == nums[-1])
