c82605ae6f61bfd6704a098f5379bbee1cab6429
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 01:21:05 +0530

    add Max End3

--- CodingBat/List-1/max_end3.py
+++ CodingBat/List-1/max_end3.py
@@ -9,3 +9,10 @@ def max_end3(nums):
   larger = max(nums[0],nums[-1])
   ChangeArray = [larger,larger,larger]
   return ChangeArray
+
+# Alternate solution
+def max_end3(nums):
+  if nums[0] > nums[-1]:
+    return [nums[0],nums[0],nums[0]]
+  else:
+    return [nums[-1],nums[-1],nums[-1]]
