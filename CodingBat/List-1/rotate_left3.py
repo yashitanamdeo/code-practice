e70192523d3a23a27e8933ad37e0c897dccd2b73
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-27 23:30:59 +0530

    add Rotate Left3

--- CodingBat/List-1/rotate_left3.py
+++ CodingBat/List-1/rotate_left3.py
@@ -4,3 +4,8 @@
 rotate_left3([1, 2, 3]) → [2, 3, 1]
 rotate_left3([5, 11, 9]) → [11, 9, 5]
 rotate_left3([7, 0, 0]) → [0, 0, 7]'''
+
+def rotate_left3(nums):
+  part2 = nums[1:]
+  part1 = nums[:1]
+  return part2+part1
