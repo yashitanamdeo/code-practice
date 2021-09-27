ffe85d48df8d569ffded85ebb786067a159e33ec
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 01:29:10 +0530

    add Middle Way

--- CodingBat/List-1/middle_way.py
+++ CodingBat/List-1/middle_way.py
@@ -4,3 +4,7 @@
 middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]
 middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
 middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]'''
+
+def middle_way(a, b):
+  newArray = [a[1],b[1]]
+  return newArray
