b698258c26ea71ad1ec0162b7fcc271c90ed0d3f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 01:25:51 +0530

    add Sum2

--- CodingBat/List-1/sum2.py
+++ CodingBat/List-1/sum2.py
@@ -4,3 +4,11 @@
 sum2([1, 2, 3]) → 3
 sum2([1, 1]) → 2
 sum2([1, 1, 1, 1]) → 2'''
+
+def sum2(nums):
+  if len(nums) == 0:
+    return 0
+  elif len(nums) < 2:
+    return sum(nums)
+  else:
+    return nums[0] + nums[1]
