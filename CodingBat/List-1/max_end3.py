2fc1d6ef78116c5cb5a5a522608a8f0e60d2bb21
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 01:17:32 +0530

    add Max End3

--- CodingBat/List-1/max_end3.py
+++ CodingBat/List-1/max_end3.py
@@ -4,3 +4,8 @@
 max_end3([1, 2, 3]) → [3, 3, 3]
 max_end3([11, 5, 9]) → [11, 11, 11]
 max_end3([2, 11, 3]) → [3, 3, 3]'''
+
+def max_end3(nums):
+  larger = max(nums[0],nums[-1])
+  ChangeArray = [larger,larger,larger]
+  return ChangeArray
