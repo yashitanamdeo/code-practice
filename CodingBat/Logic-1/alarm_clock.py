3261624267ced9319f04a60e24aaa304d331234f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 20:38:23 +0530

    add Alarm Clock

--- CodingBat/Logic-1/alarm_clock.py
+++ CodingBat/Logic-1/alarm_clock.py
@@ -4,3 +4,15 @@
 alarm_clock(1, False) → '7:00'
 alarm_clock(5, False) → '7:00'
 alarm_clock(0, False) → '10:00'"""
+
+def alarm_clock(day, vacation):
+  if vacation:
+    if day==0 or day==6:
+      return "off"
+    else:
+      return "10:00"
+  else:
+    if day==0 or day==6:
+      return "10:00"
+    else:
+      return "7:00"
