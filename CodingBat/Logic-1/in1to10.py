fba71205511498380fd7582fe64b6ee1a602d3b1
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 21:51:54 +0530

    add In 1 to 10

--- CodingBat/Logic-1/in1to10.py
+++ CodingBat/Logic-1/in1to10.py
@@ -4,3 +4,9 @@
 in1to10(5, False) → True
 in1to10(11, False) → False
 in1to10(11, True) → True'''
+
+def in1to10(n, outside_mode):
+  if outside_mode:
+    return n<=1 or n>=10
+  else:
+    return n in range(1,11)
