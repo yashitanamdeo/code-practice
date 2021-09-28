24e9b104ffecc97fc206570f683aa6976d7df733
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 20:18:29 +0530

    add Squirrel Play

--- CodingBat/Logic-1/squirrel_play.py
+++ CodingBat/Logic-1/squirrel_play.py
@@ -4,3 +4,9 @@
 squirrel_play(70, False) → True
 squirrel_play(95, False) → False
 squirrel_play(95, True) → True'''
+
+def squirrel_play(temp, is_summer):
+  if is_summer:
+    return (temp>=60 and temp<=100)
+  else:
+    return (temp>=60 and temp<=90)
