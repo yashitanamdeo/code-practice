2f0597e8a828b7188ff9400c9b12ddcca1b4b034
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 21:54:03 +0530

    add Near Ten

---
+++ CodingBat/Logic-1/near_ten.py
@@ -0,0 +1,6 @@
+'''Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.
+
+
+near_ten(12) → True
+near_ten(17) → False
+near_ten(19) → True'''
