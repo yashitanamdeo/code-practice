de9c8149c81987e246151e91cf921af4f11e7412
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 20:15:19 +0530

    add Date Fashion

--- CodingBat/Logic-1/date_fashion.py
+++ CodingBat/Logic-1/date_fashion.py
@@ -4,3 +4,11 @@
 date_fashion(5, 10) → 2
 date_fashion(5, 2) → 0
 date_fashion(5, 5) → 1'''
+
+def date_fashion(you, date):
+  if you <= 2 or date <= 2:
+    return 0
+  elif you >= 8 or date >= 8:
+    return 2
+  else:
+    return 1
