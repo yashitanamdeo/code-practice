8adc6b42998936921f26e9f8af0794f67684d83f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-09-28 20:26:02 +0530

    add Caught Speeding

--- CodingBat/Logic-1/caught_speeding.py
+++ CodingBat/Logic-1/caught_speeding.py
@@ -4,3 +4,19 @@
 caught_speeding(60, False) → 0
 caught_speeding(65, False) → 1
 caught_speeding(65, True) → 0'''
+
+def caught_speeding(speed, is_birthday):
+  if is_birthday:
+    if speed <= 65:
+      return 0
+    elif (speed>=61 and speed<=85):
+      return 1
+    elif speed>=86:
+      return 2
+  else:
+    if speed <= 60:
+      return 0
+    elif (speed>=61 and speed<=80):
+      return 1
+    elif speed>=81:
+      return 2
