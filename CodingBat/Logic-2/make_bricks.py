d3e51f23be82ec7e1c3d9b3008283e981bbc930e
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-10-06 18:40:53 +0530

    add Make Bricks

--- CodingBat/Logic-2/make_bricks.py
+++ CodingBat/Logic-2/make_bricks.py
@@ -4,3 +4,10 @@
 make_bricks(3, 1, 8) → True
 make_bricks(3, 1, 9) → False
 make_bricks(3, 2, 10) → True'''
+
+def make_bricks(small, big, goal):
+  if goal>small+(big*5):
+    return False
+  if (goal%5)>small:
+    return False
+  return True
