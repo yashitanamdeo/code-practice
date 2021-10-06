fe61d4305d0294109c9870ca6a2b617b88126e44
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-10-06 18:56:24 +0530

    add Lone sum

--- CodingBat/Logic-2/lone_sum.py
+++ CodingBat/Logic-2/lone_sum.py
@@ -16,3 +16,13 @@ def lone_sum(a, b, c):
     return a
   if a==c:
     return b
+
+
+#alternate solution
+def lone_sum(a, b, c):
+  sum = 0
+  if a != b and a != c: sum += a
+  if b != a and b != c: sum += b
+  if c != a and c != b: sum += c
+  
+  return sum
