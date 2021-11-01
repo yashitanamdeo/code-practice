675d942d50fa00bff8b1c9e3ba3ee5a14a0d12dc
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-01 11:00:11 +0530

    add Double Char

--- CodingBat/String-2/double_char.py
+++ CodingBat/String-2/double_char.py
@@ -4,3 +4,9 @@
 double_char('The') → 'TThhee'
 double_char('AAbb') → 'AAAAbbbb'
 double_char('Hi-There') → 'HHii--TThheerree'"""
+
+def double_char(str):
+  string = ''
+  for i in range(len(str)):
+    string += str[i]*2
+  return string
