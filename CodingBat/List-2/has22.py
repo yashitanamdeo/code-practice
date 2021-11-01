590035aab50807a88e46dc70b7576f9678bf4309
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-01 23:44:00 +0530

    add Has22

--- CodingBat/List-2/has22.py
+++ CodingBat/List-2/has22.py
@@ -4,3 +4,9 @@
 has22([1, 2, 2]) → True
 has22([1, 2, 1, 2]) → False
 has22([2, 1, 2]) → False'''
+
+def has22(nums):
+  for i in range(len(nums)-1):
+    if nums[i] == 2 and nums[i+1] == 2:
+      return True
+  return False
