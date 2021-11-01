5636167ccf64aea09e8eee0ee1b0a7f5c7cba071
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-01 12:52:37 +0530

    add End Other

--- CodingBat/String-2/end_other.py
+++ CodingBat/String-2/end_other.py
@@ -4,3 +4,8 @@
 end_other('Hiabc', 'abc') → True
 end_other('AbC', 'HiaBc') → True
 end_other('abc', 'abXabc') → True'''
+
+def end_other(a, b):
+  a = a.lower()
+  b = b.lower()
+  return a.endswith(b) or b.endswith(a)
