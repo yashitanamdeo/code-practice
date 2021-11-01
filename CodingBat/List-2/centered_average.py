bc297e502424551f8b51bb142fe7aa18e1957052
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-01 13:35:27 +0530

    add Centered Average

--- CodingBat/List-2/centered_average.py
+++ CodingBat/List-2/centered_average.py
@@ -4,3 +4,12 @@
 centered_average([1, 2, 3, 4, 100]) → 3
 centered_average([1, 1, 5, 5, 10, 8, 7]) → 5
 centered_average([-10, -4, -2, -4, -2, 0]) → -3'''
+
+def centered_average(nums):
+  nums.remove(max(nums))
+  nums.remove(min(nums))
+  numSum = 0
+  for i in range(len(nums)):
+    numSum += nums[i]
+  average = numSum / len(nums)
+  return average
