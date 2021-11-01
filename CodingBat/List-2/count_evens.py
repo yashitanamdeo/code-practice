62fbece4cdc718a5bd13bde05f895d4a7bbc00de
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-01 13:26:04 +0530

    add Count Evens

--- CodingBat/List-2/count_evens.py
+++ CodingBat/List-2/count_evens.py
@@ -4,3 +4,10 @@
 count_evens([2, 1, 2, 3, 4]) → 3
 count_evens([2, 2, 0]) → 3
 count_evens([1, 3, 5]) → 0'''
+
+def count_evens(nums):
+  count = 0
+  for num in nums:
+    if num%2 == 0:
+      count += 1
+  return count
