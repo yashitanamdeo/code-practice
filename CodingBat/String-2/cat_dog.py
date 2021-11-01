baf0743ebcae892956abaca96f541eef6a64c66a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-01 11:52:01 +0530

    add Cat Dog

--- CodingBat/String-2/cat_dog.py
+++ CodingBat/String-2/cat_dog.py
@@ -4,3 +4,13 @@
 cat_dog('catdog') → True
 cat_dog('catcat') → False
 cat_dog('1cat1cadodog') → True'''
+
+def cat_dog(str):
+  catCount = 0
+  dogCount = 0
+  for i in range(len(str)-1):
+    if str[i:i+3] == 'cat':
+      catCount += 1
+    if str[i:i+3] == 'dog':
+      dogCount += 1
+  return catCount == dogCount
