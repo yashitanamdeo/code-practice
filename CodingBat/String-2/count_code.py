37b6f3ef155ab0142ad8b01aec8e3cc19c6360f8
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-01 12:38:57 +0530

    add Count Code

--- CodingBat/String-2/count_code.py
+++ CodingBat/String-2/count_code.py
@@ -4,3 +4,10 @@
 count_code('aaacodebbb') → 1
 count_code('codexxcode') → 2
 count_code('cozexxcope') → 2'''
+
+def count_code(str):
+  count = 0
+  for i in range(len(str)-3):
+    if str[i:i+2] == 'co' and str[i+3] == 'e':
+      count += 1
+  return count
