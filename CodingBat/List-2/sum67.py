1e034e2f274d12c9b022779f1de2fb9902050c4a
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-11-01 23:36:45 +0530

    add Sum67

--- CodingBat/List-2/sum67.py
+++ CodingBat/List-2/sum67.py
@@ -4,3 +4,20 @@
 sum67([1, 2, 2]) → 5
 sum67([1, 2, 2, 6, 99, 99, 7]) → 5
 sum67([1, 1, 6, 7, 2]) → 4'''
+
+def sum67(nums):
+  case = True
+  total = 0
+  
+  for n in nums:
+    if n == 6:
+      case = False
+  
+    if case:
+      total += n
+      continue
+    
+    if n == 7:
+      case = True
+  
+  return total  
