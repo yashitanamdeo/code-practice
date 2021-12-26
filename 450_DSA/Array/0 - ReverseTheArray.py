39419de47b98ba084b0b400a3e37f5ed9eb6b887
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-26 16:03:24 +0530

    add 450_DSA : Reverse the array

--- 450_DSA/Array/0 - ReverseTheArray.py
+++ 450_DSA/Array/0 - ReverseTheArray.py
@@ -1,1 +1,18 @@
 # Problem Statement: https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+
+# Iterative Way
+def Iterative_reverse(array):
+    start = 0
+    end = len(array) - 1
+    print(end)
+    while start < end:
+        temp = array[start]
+        array[start] = array[end]
+        array[end] = temp
+
+        start += 1
+        end -= 1
+    return array
+
+array = [1,2,3,4]
+print(Iterative_reverse(array))
