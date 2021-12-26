28003f3ac393fdfd44dd1c4475e2726c4e34a9a4
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-26 22:20:27 +0530

    update 450_DSA: Maximum and Minimum in Array

--- 450_DSA/Array/1 - Maximum&MininumInArray.py
+++ 450_DSA/Array/1 - Maximum&MininumInArray.py
@@ -1,1 +1,19 @@
 # Problem Statement: https://www.geeksforgeeks.org/maximum-and-minimum-in-an-array/
+
+# Linear Search Method - Complexity: O(n)
+def LinearSearch(array):
+    minimum = array[0]
+    maximum = array[0]
+    for element in array:
+        if element < minimum:
+            minimum = element
+        if element > maximum:
+            maximum = element
+        else:
+            pass
+    return minimum, maximum
+
+array = [5,4,9,6,7,1,3]
+Min, Max = LinearSearch(array)
+print("Min: ", Min)
+print("Max: ", Max)
