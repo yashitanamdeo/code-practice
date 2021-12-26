90363044dc2222526ae9f89a3abf85d58c1af651
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-26 15:34:48 +0530

    updated repository

--- ProjectEuler.Net/even_fibonacci_numbers.py
+++ ProjectEuler.Net/even_fibonacci_numbers.py
@@ -3,3 +3,14 @@
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
 By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.'''
+
+n1, n2 = 0, 1
+count = 0
+numbers = []
+while count < 4000000:
+    numbers.append(n1)
+    nth = n1 + n2
+    n1 = n2
+    n2 = nth
+    count += 1
+print(sum(numbers))
